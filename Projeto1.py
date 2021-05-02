9c4d6748f1778dd04361f9b306bf622c7e910ba4
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-05-02 13:44:38 -0300

    Primeiros testes práticos

---
+++ Projeto1.py
@@ -0,0 +1,20 @@
+
+#Aqui estou criando um comentário!
+
+'''Aqui estou criando
+	várias linhas
+	de comentário'''
+
+print('Hello, world!!') #de praxe
+
+a = 5
+b = 5.0
+c = a / 2
+d = b // 2
+e = a % 2
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
