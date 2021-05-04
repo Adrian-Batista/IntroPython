667e0216e4e54a31ed39f42422a35e394dfeaff6
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-05-03 21:01:31 -0300

    Create lista-102.py

---
+++ lista-102.py
@@ -0,0 +1,55 @@
+
+# Resolvendo as atividades do módulo 102
+
+weekdays = ['mon','tues','wed','thurs','fri']
+
+list = ['a', 1, 3.14159265359]
+
+#########
+# Exercicios - Listas
+# Faca sem usar loops
+#########
+
+# Como selecionar 'wed' pelo indice?
+
+weekdays[2]
+
+# Como verificar o tipo de 'mon'?
+
+type(weekdays[0])
+
+# Como separar 'wed' até 'fri'?
+
+weekdays[2:5]
+
+# Quais as maneiras de selecionar 'fri' por indice?
+
+weekdays[-1]
+
+weekdays[4]
+
+# Qual eh o tamanho dos dias e days_list?
+
+len(weekdays)
+
+# Como inverter a ordem dos dias?
+
+weekdays.reverse()
+
+weekdays[::-1]
+
+# Como inserir a palavra 'zero' entre 'a' e 1 de list?
+
+list.insert(1, 'zero')
+
+# Como limpar list?
+
+list.clear()
+
+# Como deletar list?
+
+del(list)
+
+# Como atribuir o ultimo elemento de list na variavel ultimo_elemento e remove-lo de list?
+ultimo_elemento = list[-1]
+list.pop()
